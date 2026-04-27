14f02e0a1bf4f2e5a155da961117efbb85981a7c
CedrickPro <cedricktacan07@gmail.com>
2026-04-27 12:21:50 +0800

    Upload app/Models/Attendance.php

---
+++ app/Models/Attendance.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Attendance extends Model
+{
+    use HasFactory;
+
+    protected $table = 'attendance';
+
+    protected $fillable = [
+        'student_id',
+        'section_id',
+        'date',
+        'status',
+    ];
+
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
+
+    public function section()
+    {
+        return $this->belongsTo(Section::class);
+    }
+}
